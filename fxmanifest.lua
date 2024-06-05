0d35be02511a93e2d539f4aed7171a73c155e6ec
Diogo Gramunha Silva <128035019+DiogoGramunha@users.noreply.github.com>
2024-06-05 14:49:05 +0100

    fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5' 
+lua54 'yes'
+
+author 'YODDA THINGS'
+description 'Combine items with ox_inventory'
+version '1.0'
+
+server_scripts {
+	'server.lua'
+}
+
+client_scripts {
+	'client.lua'
+}
+
+
+dependencies { 'ox_lib', 'ox_inventory'}
+
+shared_scripts {'@ox_lib/init.lua'}
